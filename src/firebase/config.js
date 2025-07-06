d4110f42442f5d73a842fdff4788ebe570b5e6a1
Ankush Sharma <ankushshharma2@gmail.com>
2025-07-06 06:07:30 +0530

    Move Firebase config to .env and update config.js for Vite

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,20 +3,21 @@ import { getAuth } from 'firebase/auth';
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC5tlTfGUTFPQpyLeritlR7p_GI1d1dXvU",
-    authDomain: "zillout-assessment.firebaseapp.com",
-    projectId: "zillout-assessment",
-    storageBucket: "zillout-assessment.firebasestorage.app",
-    messagingSenderId: "581951519551",
-    appId: "1:581951519551:web:73ca24c440c020950b718a",
-    measurementId: "G-B33DZH61MH"
-  };
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+};
+
   
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-console.log(analytics);
+console.log(analytics); //debug
 
 // Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
